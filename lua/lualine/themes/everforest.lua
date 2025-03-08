a6945cda27963d3b5e73c0b4428a8905ab142d51
wux4an <wux4an@duck.com>
2025-03-07 21:21:51 -0500

    change text color for foreground

--- lua/lualine/themes/everforest.lua
+++ lua/lualine/themes/everforest.lua
@@ -9,32 +9,32 @@ end
 
 return {
   normal = {
-    a = { bg = palette.statusline1, fg = palette.bg0, gui = "bold" },
+    a = { bg = palette.statusline1, fg = "#1e2326", gui = "bold" },
     b = { bg = palette.bg3, fg = palette.grey2 },
     c = { bg = palette.bg1, fg = palette.grey1 },
   },
   insert = {
-    a = { bg = palette.statusline2, fg = palette.bg0, gui = "bold" },
+    a = { bg = palette.statusline2, fg = "#1e2326", gui = "bold" },
     b = { bg = palette.bg3, fg = palette.fg },
     c = { bg = palette.bg1, fg = palette.fg },
   },
   visual = {
-    a = { bg = palette.statusline3, fg = palette.bg0, gui = "bold" },
+    a = { bg = palette.statusline3, fg = "#1e2326", gui = "bold" },
     b = { bg = palette.bg3, fg = palette.fg },
     c = { bg = palette.bg1, fg = palette.fg },
   },
   replace = {
-    a = { bg = palette.orange, fg = palette.bg0, gui = "bold" },
+    a = { bg = palette.orange, fg = "#1e2326", gui = "bold" },
     b = { bg = palette.bg3, fg = palette.fg },
     c = { bg = palette.bg1, fg = palette.fg },
   },
   command = {
-    a = { bg = palette.aqua, fg = palette.bg0, gui = "bold" },
+    a = { bg = palette.aqua, fg = "#1e2326", gui = "bold" },
     b = { bg = palette.bg3, fg = palette.fg },
     c = { bg = palette.bg1, fg = palette.fg },
   },
   terminal = {
-    a = { bg = palette.purple, fg = palette.bg0, gui = "bold" },
+    a = { bg = palette.purple, fg = "#1e2326", gui = "bold" },
     b = { bg = palette.bg3, fg = palette.fg },
     c = { bg = palette.bg1, fg = palette.fg },
   },
